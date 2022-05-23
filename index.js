37f8fdd522610c4d37483bec059566a5f3c54686
DelowarHossain69 <daloar2400@gmail.com>
2022-05-23 09:10:51 -0700

    jwt token middleware implemented

--- index.js
+++ index.js
@@ -1,51 +1,91 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const app = express();
 const PORT = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
+const verifyToken = (req, res, next) => {
+  const email = req.query.email;
+  console.log(email);
+  const auth = req.headers.auth;
+  if (!auth) {
+    return res.status(401).send({ message: "Unauthorized user" });
+  }
+
+  const token = auth.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      res.status(403).send({ message: "Forbidden access" });
+    }
+
+    const decodedEmail = decoded.email;
+
+    if (email === decodedEmail) {
+      next();
+    } else {
+      res.status(403).send({ message: "Forbidden access" });
+    }
+  });
+};
+
 // default route
-app.get('/', (req, res)=>{
-    res.send('hello world');
+app.get("/", (req, res) => {
+  res.send("hello world");
 });
 
 // Mongodb interface
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuivm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        const productCollection = client.db('Aronic_hardware_shop').collection('products');
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client
+      .db("Aronic_hardware_shop")
+      .collection("products");
+    const userCollection = client
+      .db("Aronic_hardware_shop")
+      .collection("products");
 
-        // get products
-        app.get('/resent-products', async(req, res)=>{
-            const productCount = await productCollection.estimatedDocumentCount();
-            const products = productCollection.find();
-            if(productCount > 6){
-                const result = await products.skip(productCount - 6).toArray();
-                res.send(result);
-            }
-            else{
-                const result = await products.toArray();
-                res.send(result);
-            }
-        });
-    }
-    finally{
+    // get products
+    app.get("/resent-products", async (req, res) => {
+      const productCount = await productCollection.estimatedDocumentCount();
+      const products = productCollection.find();
+      if (productCount > 6) {
+        const result = await products.skip(productCount - 6).toArray();
+        res.send(result);
+      } else {
+        const result = await products.toArray();
+        res.send(result);
+      }
+    });
 
-    }
+    /**
+     *
+     * User manage
+     *
+     */
+
+   
+  } 
+  finally {
+  }
 }
 
 run().catch(console.dir);
 
 // The app is listening 5000 port
-app.listen(PORT, ()=>{
-    console.log('The app is running.');
-})
+app.listen(PORT, () => {
+  console.log("The app is running.");
+});
